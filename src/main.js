f12fd3e0e9f27cde6f52a9954ef8d924bfee81e6
dima_legan <dimasky7@yandex.ru>
2021-11-26 20:54:48 +0700

    list of films and popup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import {createSiteMenuTemplate} from './view/site-menu-view.js';
+import { createFilmCardPopup } from './view/film-card-popup.js';
 import {renderTemplate, RenderPosition} from './render.js';
 
 const siteMainElement = document.querySelector('.main');
+const popup = document.querySelector('body');
 
 renderTemplate(siteMainElement, createSiteMenuTemplate(), RenderPosition.BEFOREEND);
+renderTemplate(popup, createFilmCardPopup(), RenderPosition.BEFOREEND);
