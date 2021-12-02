83e348a520dc89b4e04f9f83cad103641dcb9ead
dima_legan <dimasky7@yandex.ru>
2021-12-03 02:04:21 +0700

    module1-task3. Finish

--- src/main.js
+++ src/main.js
@@ -5,17 +5,24 @@ import { createProfileRating } from './view/profile-rating-view.js';
 import { createProfileStatistics } from './view/profile-statistics-view.js';
 import { createMainMenu } from './view/main-menu-view.js';
 import { createShowMoreButton } from './view/show-more-button-view.js';
+import { createNumberOfMovies } from './view/number-of-movies-view.js';
 
+const FILM_CARD_COUNT = 5;
 
 const profileRating = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
-//const popup = document.querySelector('body');
+const filmList = siteMainElement.querySelector('.films-list');
+const filmCardContainer = filmList.querySelector('.films-list__container');
+const numberOfMovies = document.querySelector('.footer__statistics');
+const popup = document.querySelector('body');
 
 
 renderTemplate(profileRating, createProfileRating(), RenderPosition.BEFOREEND);
 renderTemplate(siteMainElement, createMainMenu(), RenderPosition.AFTERBEGIN);
-renderTemplate(siteMainElement, createFilmCard(), RenderPosition.BEFOREEND);
-renderTemplate(siteMainElement, createShowMoreButton(), RenderPosition.BEFOREEND);
+for (let i=0; i < FILM_CARD_COUNT; i++) {
+  renderTemplate(filmCardContainer, createFilmCard(), RenderPosition.BEFOREEND);
+}
+renderTemplate(filmList, createShowMoreButton(), RenderPosition.BEFOREEND);
 renderTemplate(siteMainElement, createProfileStatistics(), RenderPosition.BEFOREEND);
-//renderTemplate(popup, createFilmCardPopup(), RenderPosition.BEFOREEND);
-
+renderTemplate(numberOfMovies, createNumberOfMovies(), RenderPosition.BEFOREEND);
+renderTemplate(popup, createFilmCardPopup(), RenderPosition.BEFOREEND);
