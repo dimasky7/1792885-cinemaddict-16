2bc43385df0e9f350cb7129ae753b553a5d4dc0c
dima_legan <dimasky7@yandex.ru>
2021-12-16 02:18:30 +0700

    module3-task1. Working project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import {renderTemplate, renderElement, RenderPosition} from './render.js';
+import {render, RenderPosition} from './render.js';
 import UserProfile from './view/user-profile-view.js';
 import FiltersView from './view/filters-view.js';
 import SortView from './view/sort-view.js';
@@ -10,7 +10,6 @@ import PopupView from './view/card-detail-popup-view.js';
 import {getMovie} from './mock/movie.js';
 import {getComments} from './mock/comments.js';
 import {getFilters} from './mock/filters.js';
-import {START_INDEX} from './const.js';
 
 const CARD_IN_LIST_COUNT = 23;
 const MOVIE_COUNT_PER_STEP = 5;
@@ -23,62 +22,73 @@ const filters = getFilters(movies);
 const headerElement = document.querySelector('.header');
 const mainElement = document.querySelector('.main');
 
-const createSomeFilmCards = (count, container) => {
-  for (let i = 0; i < count; i++) {
-    const cardViewComponent = [];
-    cardViewComponent[i] = new CardView(movies[i]);
-    renderElement(container, cardViewComponent[i].element, RenderPosition.BEFOREEND);
-    cardViewComponent[i].link.addEventListener('click', () => {
-      const popup = new PopupView(movies[i], comments);
-      document.body.appendChild(popup.element);
-      document.body.classList.add('hide-overflow');
-      popup.closePopup.addEventListener('click', () => {
-        document.body.removeChild(popup.element);
-        document.body.classList.remove('hide-overflow');
-      });
-    });
-  }
-};
-
-renderElement(headerElement, new UserProfile(movies).element, RenderPosition.BEFOREEND);
-renderElement(mainElement, new FiltersView(filters).element, RenderPosition.BEFOREEND);
-renderElement(mainElement, new SortView().element, RenderPosition.BEFOREEND);
-renderElement(mainElement, new ListView().element, RenderPosition.BEFOREEND);
+render(headerElement, new UserProfile(movies).element, RenderPosition.BEFOREEND);
+render(mainElement, new FiltersView(filters).element, RenderPosition.BEFOREEND);
+render(mainElement, new SortView().element, RenderPosition.BEFOREEND);
+render(mainElement, new ListView().element, RenderPosition.BEFOREEND);
 
 const filmElement = document.querySelector('.films');
 const filmListElement = filmElement.querySelector('.films-list');
 const filmContainerElement = filmListElement.querySelector('.films-list__container');
 
-createSomeFilmCards(MOVIE_COUNT_PER_STEP, filmContainerElement);
-
 const footer = document.querySelector('.footer');
 const statisticsContainerElement = footer.querySelector('.footer__statistics');
+render(statisticsContainerElement, new StatsView(movies).element, RenderPosition.BEFOREEND);
 
-renderElement(statisticsContainerElement, new StatsView(movies).element, RenderPosition.BEFOREEND);
 
-//comment next line to check the program
-//renderTemplate(footer, createCardDetailPopup(movies[START_INDEX], comments), RenderPosition.AFTEREND);
+const renderCards = (cardContainer, moviesArray) => {
+  const cardViewComponent = [];
+  let i = -1;
+  moviesArray
+    .slice(0, Math.min(moviesArray.length, MOVIE_COUNT_PER_STEP))
+    .forEach((movie) => {
+      i++;
+      cardViewComponent[i] = new CardView(movie);
+      render(cardContainer, cardViewComponent[i].element, RenderPosition.BEFOREEND);
+      cardViewComponent[i].link.addEventListener('click', () => {
+        const popup = new PopupView(movie, comments);
+        document.body.appendChild(popup.element);
+        document.body.classList.add('hide-overflow');
+        popup.closePopup.addEventListener('click', () => {
+          document.body.removeChild(popup.element);
+          document.body.classList.remove('hide-overflow');
+        });
+      });
+    });
 
-if (movies.length > MOVIE_COUNT_PER_STEP) {
-  let renderedTaskCount = MOVIE_COUNT_PER_STEP;
 
-  //renderTemplate(filmListElement, createShowMoreButtonTemplate(), RenderPosition.BEFOREEND);
-  const showMoreButtonComponent = new ShowMoreButtonView();
-  renderElement(filmListElement, showMoreButtonComponent.element, RenderPosition.BEFOREEND);
+  if (movies.length > MOVIE_COUNT_PER_STEP) {
+    let renderedMovieCount = MOVIE_COUNT_PER_STEP;
 
-  //const loadMoreButton = filmListElement.querySelector('.films-list__show-more');
+    const showMoreButtonComponent = new ShowMoreButtonView();
+    render(filmListElement, showMoreButtonComponent.element, RenderPosition.BEFOREEND);
 
-  showMoreButtonComponent.element.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    movies
-      .slice(renderedTaskCount, renderedTaskCount + MOVIE_COUNT_PER_STEP)
-      .forEach((movie) => renderElement(filmContainerElement, new CardView(movie).element, RenderPosition.BEFOREEND));
+    showMoreButtonComponent.element.addEventListener('click', () => {
+      moviesArray
+        .slice(renderedMovieCount, renderedMovieCount + MOVIE_COUNT_PER_STEP)
+        .forEach((movie) => {
+          i++;
+          cardViewComponent[i] = new CardView(movie);
+          render(cardContainer, cardViewComponent[i].element, RenderPosition.BEFOREEND);
+          cardViewComponent[i].link.addEventListener('click', () => {
+            const popup = new PopupView(movie, comments);
+            document.body.appendChild(popup.element);
+            document.body.classList.add('hide-overflow');
+            popup.closePopup.addEventListener('click', () => {
+              document.body.removeChild(popup.element);
+              document.body.classList.remove('hide-overflow');
+            });
+          });
+        });
 
-    renderedTaskCount += MOVIE_COUNT_PER_STEP;
+      renderedMovieCount += MOVIE_COUNT_PER_STEP;
 
-    if (renderedTaskCount >= movies.length) {
-      showMoreButtonComponent.element.remove();
-      showMoreButtonComponent.removeElement();
-    }
-  });
-}
+      if (renderedMovieCount >= movies.length) {
+        showMoreButtonComponent.element.remove();
+        showMoreButtonComponent.removeElement();
+      }
+    });
+  }
+};
+
+renderCards(filmContainerElement, movies);
